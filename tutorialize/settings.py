4fa274ed1686cf0fb2005201609fba1b8301314a
Guy Davidson <guydav@gmail.com>
2018-02-04 14:30:13 +0530

    Added second email address

--- tutorialize/settings.py
+++ tutorialize/settings.py
@@ -136,6 +136,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Email-related stuff
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'minerva.tutorialize@gmail.com'
-EMAIL_HOST_PASSWORD = 'MinervaTutorialize1!'
+# EMAIL_HOST_USER = 'minerva.tutorialize@gmail.com'
+EMAIL_HOST_USER = 'minerva.tutorialize.2@gmail.com'
+# EMAIL_HOST_PASSWORD = 'MinervaTutorialize1!'
+EMAIL_HOST_PASSWORD = 'MinervaTutorialize2@'
 EMAIL_PORT = 587
